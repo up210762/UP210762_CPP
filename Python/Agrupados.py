0218a2b0700dd217886f80fb8828865817bce581
up210762 <up210762@alumnos.upa.edu.mx>
2022-09-29 05:48:39 -0500

    Actualización

--- Python/Agrupados.py
+++ Python/Agrupados.py
@@ -1,5 +1,7 @@
-import cmath
+import numpy as np
 import math as ma
+import fnmatch
+import matplotlib
 
 # Variables estáticas
 longitud = int(input("¿Cuántos números introduces? "))
@@ -9,8 +11,10 @@ limInf = []
 limSup = []
 ranCat = []
 frecI = []
+FreAc = []
 hi = []
 Acmxi = []
+mapa_cantidades={}
 VzaAc = 0
 frecIa = 0
 xi = 0
@@ -28,9 +32,10 @@ xmax = 0
 xmin = 0
 
 # Escritura de los valores
+print("Introduce los valores: ")
 for i in range(longitud):
     Vmin = 0
-    números = int(input("Introduce los números: "))
+    números = int(input("> "))
     Datos.append(números)
 
 #Selección del número máximo
@@ -48,7 +53,7 @@ xmin = Acomodo[0]
 Rango = xmax - xmin
 
 #Regla de Sturges
-Logaritmo = ma.log10(longitud)
+Logaritmo = round(ma.log10(longitud), 2)
 crudCat = 1 + 3.322*Logaritmo
 Categorias = round(crudCat)
 crudAmp = Rango/crudCat
@@ -93,10 +98,11 @@ for i in range(valor):
 valor = len(frecI)
 for i in range(valor):
     frecIa = frecIa + frecI[i]
+    FreAc.append(frecIa)
 
 #Frecuencia relativa
 for i in range(valor):
-    Val = frecI[i]/longitud
+    Val = round(frecI[i]/longitud, 2)
     frecRe.append(Val)
 #Frecuemcia relativa acumulada
     frecReA = frecReA + Val
@@ -108,14 +114,60 @@ for i in range(valor):
 
 #Varianza
 for i in range(valor):
-    Val = Val + (Acmxi[i]*frecI[i])
-x = Val/longitud
+    Val = Val + round((Acmxi[i]*frecI[i]), 4)
+x = round(Val/longitud, 4)
 for i in range(valor):
-    VzaAc = VzaAc + (((xi[i]-x)^2)*frecI)
-Vza = VzaAc/(longitud-1)
+    VzaAc = VzaAc + round(((Acmxi[i]-x)*(Acmxi[i]-x))*frecI[i], 4)
+    
+Vza = round(VzaAc/(longitud-1), 4)
 
 #Desviación estándar
-Sd = cmath.sqrt(Vza)
+Sd = round((np.sqrt(Vza)), 4)
+
+#Límite inferior
+linMax = 0
+for i in range(valor):
+    if linMax < frecI[i]:
+        linMax = limInf[i]
+        xmax = frecI[i]
+        if (i - 1) < 0:
+            valor = 0
+            Vmin = 0
+        else:
+            valor = limInf[i-1]
+            Vmin = frecI[i-1]
+            Vmax = frecI[i+1]
+    
+
+#Media
+
+
+
+#Mediana
+Val = (((longitud/2)-valor)/sup)
+Med = linMax + Amplitud*Val
+
+
+#Moda
+Val = ((xmax-Vmin)/((xmax-Vmin)+(xmax-Vmax)))
+Mod = linMax + Amplitud*Val
+
+#Cuartiles
+
+#Impresión de gráficas
+print("1) histograma\n2) caja y bigote\n3) de puntos")
+enter = input()
+while enter < 4 and enter > 0:
+    if enter == "1":
+        for i in Acomodo:
+            if i in mapa_cantidades:
+                mapa_cantidades[i] += 1
+            else:
+                mapa_cantidades[i] = 1
+        for j in sorted(mapa_cantidades):
+            print(f'{j}: {mapa_cantidades[j]}')
+
+
 
 #Impresiones de pantalla
 for i in range(valor):
@@ -124,3 +176,4 @@ print(f"Frecuecia absoluta: {frecI}\nFrecuencia absoluta acumulada: {frecIa}")
 print(f"Frecuencia relativa: {frecRe}\nFrecuencia relativa acumulada: {frecReA}")
 print(f"Ampitud: {Amplitud}\nCategoría: {ranCat}\nValores: {Acomodo}")
 print(f"Varianza: {Vza}\nDesviación estándar: {Sd}")
+print(f"Limite inferior: {linMax}")
