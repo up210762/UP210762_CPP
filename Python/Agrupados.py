dd75433e23d4bde304dd01a89ebaf8e41e5a66c2
up210762 <up210762@alumnos.upa.edu.mx>
2022-09-28 21:34:34 -0500

    Actualización

--- Python/Agrupados.py
+++ Python/Agrupados.py
@@ -1,27 +1,34 @@
 import cmath
 import math as ma
-import tkinter as tk
 
-# Medidas de tendencia central
-
+# Variables estáticas
 longitud = int(input("¿Cuántos números introduces? "))
-Sumatoria = 0
-Datos = []
+frecRe = 0
+frecReA = 0
 xmax = 0
-Vmax = 0
 xmin = 0
-Vmin = 0
-Val = 0
+Datos = []
 limInf = []
 limSup = []
-sup = 0
-Rango = 0
 ranCat = []
+frecI = []
+frecIa = []
+sup = 0
+xi = 0
+Rango = 0
 
+# Variables dinámicas
+valor = 0
+Val = 0
+Vmin = 0
+Vmax = 0
+
+# Escritura de los valores
 for i in range(longitud):
     Vmin = 0
     números = int(input("Introduce los números: "))
     Datos.append(números)
+
 #Selección del número máximo
     if números > xmax:
         xmax = números
@@ -35,40 +42,55 @@ xmin = Acomodo[0]
 
 #Rango
 Rango = xmax - xmin
+
 #Regla de Sturges
 Logaritmo = ma.log10(longitud)
 crudCat = 1 + 3.322*Logaritmo
 Categorias = round(crudCat)
 crudAmp = Rango/crudCat
 Amplitud = round(crudAmp)
+valor = Categorias-1
 
 for i in range(Categorias):
     if i == 0:
         limInf.append(xmin)
         limSup.append(xmin+Amplitud)
-        ranCat.append(f"{limInf},{limSup}")
     elif i != 0:
         limInf.append(limSup[i-1])
         limSup.append(limInf[i]+Amplitud)
-    elif limSup[Categorias] <= xmax:
-        limInf.append(limSup)
-        limSup.append(limSup+Amplitud)
-    
-print(f"Ampitud: {Amplitud}\nLímite inferior: {limInf}\nLímite superior: {limSup}\nCategoría: {ranCat}")
 
+if limSup[valor] <= xmax:
+    Vmax = limSup[valor]
+    limInf.append(Vmax)
+    limSup.append(Vmax+Amplitud)
 
+#Obtener categorías
+valor = len(limInf)
+Val = len(Acomodo)
+for i in range(valor):
+    fi = 0
+    xi = (limSup[i]+limInf[i])/2
+    ranCat.append([limInf[i], limSup[i]])
+    print(xi)
 
-'''
-Media = Sumatoria/longitud
+#Frecuencia absoluta
+for i in range(valor):
+    fi = 0
+    Vmin = limInf[i]
+    Vmax = limSup[i]
+    for j in range(Val):
+        if Acomodo[j] < Vmax and Acomodo[j] >= Vmin:
+            fi = fi + 1
+    frecI.append(fi)
 
+#Frecuencia absoluta acumulada
+valor = len(frecI)
+for i in range(valor):
+    frecIa = frecIa + frecI(i)
 
+#Frecuencia relativa
+for i in range(valor):
+    frecRe = frecI[i]/longitud
 
-if Categorias != int:
-
-
-Categorias
-Amplitud = longitud/Categorias
-
-
-Varianza = 
-'''
+print(f"Frecuecia absoluta: {frecI}")
+print(f"Ampitud: {Amplitud}\nLímite inferior: {limInf}\nLímite superior: {limSup}\nCategoría: {ranCat}\nValores: {Acomodo}")
