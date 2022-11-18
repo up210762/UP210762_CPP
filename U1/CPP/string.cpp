c975b5ea331c9b8ff31accb654cd11d611fa2bbc
up210762 <up210762@alumnos.upa.edu.mx>
2022-11-18 13:12:44 -0600

    Programas cpp

---
+++ U1/CPP/string.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    char a[] = "sida";
+    string texto = "Universidad UPA";
+    string texto2("HolA");
+
+    cout << "contenido: " << texto << endl;
+    cout << "length: " << texto.length() << endl;
+    cout << "compare: " << texto.compare("Universidad") << endl;
+    cout << "find: " << texto.find(a) << endl;
+    cout << "substr: " << texto.substr(6,4) << endl;
+    cout << "find_last: " << texto.find_last_of("i") << endl;
+    cout << "replace: " << texto.replace(6,5,"wih") << endl;
+    texto = texto + ".";
+
+}
